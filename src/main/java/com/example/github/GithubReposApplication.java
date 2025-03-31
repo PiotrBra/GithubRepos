dde9233ec82d3e9ad71ca95c7c49a57f1d2aafe5
Piotr Branewski <pbranewski@student.agh.edu.pl>
2025-03-31 20:45:43 +0200

    main add rest template

--- src/main/java/com/example/github/GithubReposApplication.java
+++ src/main/java/com/example/github/GithubReposApplication.java
@@ -2,12 +2,19 @@ package com.example.github;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class GithubReposApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GithubReposApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 }
