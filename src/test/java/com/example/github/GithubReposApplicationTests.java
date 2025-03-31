bc1538f8569fe58078b633e5febc4047b4bb417a
Piotr Branewski <pbranewski@student.agh.edu.pl>
2025-03-31 19:29:19 +0200

    Initial commit

---
+++ src/test/java/com/example/github/GithubReposApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubReposApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
